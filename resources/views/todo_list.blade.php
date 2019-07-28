30e43cfcb97f1d147fc148f840ecc8dfd6c242c4
NeverMine17 <dannevergame@gmail.com>
2019-07-29 00:32:07 +0400

    HTML formatting

--- resources/views/todo_list.blade.php
+++ resources/views/todo_list.blade.php
@@ -1,28 +1,34 @@
-<html>
+<!DOCTYPE html>
+<html lang="ru">
+    <head>
+        <title>Todo List</title>
+    </head>
     <body>
         <ul>
             @foreach ($data as $todo)
             <li>{{$todo->id}}. {{ $todo->data }}</li>
             @endforeach
         </ul>
-        <form method="POST">
-            @csrf
-            <input type="text" name="data" placeholder="текст"/>
-            <button type="submit">Add</button>
-        </form>
-        <form method="POST">
-            @csrf
-            <input type="hidden" name="del" value="true">
-            <input type="text" name="id" placeholder="ID элемента"/>
-            <button type="submit">Delete</button>
-        </form>
-        <form method="POST">
-            @csrf
-            <input type="hidden" name="edit" value="true">
-            <input type="text" name="id" placeholder="ID элемента">
-            <input type="text" name="data" placeholder="текст">
-            <button type="submit">Edit</button>
-        </form>
+        <div>
+            <form method="POST">
+                @csrf
+                <input type="text" name="data" placeholder="текст"/>
+                <button type="submit">Add</button>
+            </form>
+            <form method="POST">
+                @csrf
+                <input type="hidden" name="del" value="true">
+                <input type="text" name="id" placeholder="ID элемента"/>
+                <button type="submit">Delete</button>
+            </form>
+            <form method="POST">
+                @csrf
+                <input type="hidden" name="edit" value="true">
+                <input type="text" name="id" placeholder="ID элемента">
+                <input type="text" name="data" placeholder="текст">
+                <button type="submit">Edit</button>
+            </form>
+        </div>
         <script>
             if ( window.history.replaceState ) {
                 window.history.replaceState( null, null, window.location.href );
